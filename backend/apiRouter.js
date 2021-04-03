38ef3ed36f887154b2fbb50dc7e60b5d8149c350
Michael <icimike@hotmail.com>
2021-04-03 10:11:15 +0200

    Create & Show message

--- backend/apiRouter.js
+++ backend/apiRouter.js
@@ -1,6 +1,7 @@
 // Imports
 const express = require('express');
 const usersCtrl = require('./routes/usersCtrl');
+const messagesCtrl = require('./routes/messagesCtrl');
 
 // Router
 exports.router = (function() {
@@ -11,6 +12,10 @@ exports.router = (function() {
     apiRouter.route('/users/login/').post(usersCtrl.login);
     apiRouter.route('/users/me/').get(usersCtrl.getUserProfile);
     apiRouter.route('/users/me/').put(usersCtrl.updateUserProfile);
+
+    // Messages routes
+    apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
+    apiRouter.route('/messages/').get(messagesCtrl.listMessages);
    
 
     return apiRouter;
--- backend/models/message.js
+++ backend/models/message.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Message.init({
-    idUSERS: DataTypes.INTEGER,
+    // idUSERS: DataTypes.INTEGER,
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     attachment: DataTypes.STRING,
