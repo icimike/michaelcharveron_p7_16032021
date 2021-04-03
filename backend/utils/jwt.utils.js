d10405d28ddbcfedef4de4fa4feda3632fbea733
Michael <icimike@hotmail.com>
2021-04-03 09:37:50 +0200

    Get & Edit user

--- backend/utils/jwt.utils.js
+++ backend/utils/jwt.utils.js
@@ -4,14 +4,29 @@ const JWT_SIGN_SECRET = '05dfgbf6tmwpadn84toxnz861anj0dejvmm';
 
 // Exported functions
 module.exports = {
-    generateTokenForUser: function(userData) {
-        return jwt.sign({
-            userId: userData.id,
-            isAdmin: userData.isAdmin
-        },
-        JWT_SIGN_SECRET,
-        {
-            expiresIn: '24h'
-        })
+  generateTokenForUser: function(userData) {
+    return jwt.sign({
+      userId: userData.id,
+      isAdmin: userData.isAdmin
+    },
+    JWT_SIGN_SECRET,
+    {
+      expiresIn: '24h'
+    })
+  },
+  parseAuthorization: function(authorization) {
+    return (authorization != null) ? authorization.replace('Bearer ', '') : null;
+  },
+  getUserId: function(authorization) {
+    var userId = -1;
+    var token = module.exports.parseAuthorization(authorization);
+    if(token != null) {
+      try {
+        var jwtToken = jwt.verify(token, JWT_SIGN_SECRET);
+        if(jwtToken != null)
+          userId = jwtToken.userId;
+      } catch(err) { }
     }
+    return userId;
+  }
 }
